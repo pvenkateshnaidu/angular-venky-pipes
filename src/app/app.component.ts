09f22a893353c355f0ce6f7991a42dc9c1d62f9d
pvenkateshnaidu <pvenkateshnaidu@gmail.com>
2022-04-12 22:26:45 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'Angular Pipes ';
+  birthday = new Date();
+  heroes: any[] = [];
+  canFly = true;
+
+  constructor() {
+    this.reset();
+  }
+  addHero(name: string) {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    let hero = { name, canFly: this.canFly };
+    this.heroes.push(hero);
+  }
+  reset() {
+    this.heroes = HEROES.slice();
+  }
 }
